b775b8d24a36bb28837750deb18b049dfd9d3594
luist23 <00127717@uca.edu.sv>
2020-05-02 21:19:09 -0600

    add controller'

---
+++ src/main/java/com/uca/capas/modelo/controller/MainController.java
@@ -0,0 +1,36 @@
+package com.uca.capas.modelo.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.uca.capas.modelo.domain.Producto;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/producto")
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("producto", new Producto());
+		mav.setViewName("index");
+		return mav;
+	}	
+	
+	@RequestMapping("/procesar")
+	public ModelAndView procesar(@Valid @ModelAttribute Producto producto , BindingResult result) {
+		ModelAndView mav = new ModelAndView();
+		
+		if(result.hasErrors()) { 
+			mav.setViewName("/index");
+		}
+		else { 
+			mav.setViewName("/result");
+		}
+		return mav;
+	}
+}
