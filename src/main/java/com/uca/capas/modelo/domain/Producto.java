7d5b9b35fe42836936240b1770e26ddc08a12d17
luist23 <00127717@uca.edu.sv>
2020-05-02 21:09:50 -0600

    generate product

--- src/main/java/com/uca/capas/modelo/domain/Producto.java
+++ src/main/java/com/uca/capas/modelo/domain/Producto.java
@@ -22,6 +22,54 @@ public class Producto {
 	Integer existencias;
 	
 	@Pattern(regexp = "^(((0[1-9]|[12][0-9]|3[01])[- /.](0[13578]|1[02])|(0[1-9]|[12][0-9]|30)[- /.](0[469]|11)|(0[1-9]|1\\d|2[0-8])[- /.]02)[- /.]\\d{4}|29[- /.]02[- /.](\\d{2}(0[48]|[2468][048]|[13579][26])|([02468][048]|[1359][26])00))$", message = "La fecha debe ser ingresada de esta manera: dd/mm/yyyy")
-	String fechaIngreso;
+	String fecha;
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Integer getExistencias() {
+		return existencias;
+	}
+
+	public void setExistencias(Integer existencias) {
+		this.existencias = existencias;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
 
 }
